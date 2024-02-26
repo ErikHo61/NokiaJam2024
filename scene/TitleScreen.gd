f6f428c68073fc4e8ef63cbe4c623fbb93c798f4
Erik H <eho61@my.bcit.ca>
2024-02-25 20:44:44 -0800

    Added Title Screen and Handling

--- scene/TitleScreen.gd
+++ scene/TitleScreen.gd
@@ -3,8 +3,8 @@ extends Node2D
 var options = ["Start", "Exit"]
 var selector = 0
 
-func _input(event):
-	if event.is_action_pressed("kp 2") || event.is_action_pressed("kp 8"): # down
+func _process(delta):
+	if(Input.is_action_just_pressed("kp 8") || Input.is_action_just_pressed("kp 2")):
 		if selector==0:
 			selector=1
 			var child := $Start as RichTextLabel
@@ -19,9 +19,31 @@ func _input(event):
 			var child2 := $Exit as RichTextLabel
 			child2.text = "Exit"
 			#print("selector is %d", selector )
-		
-	if event.is_action_pressed("kp enter"): # enter
-			if selector==0:
-				get_tree().change_scene_to_file("res://scene/Game.tscn")
-			else:
-				get_tree().quit()
+	if(Input.is_action_just_pressed("kp enter")):
+		if selector==0:
+			get_tree().change_scene_to_file("res://maingame.tscn")
+		else:
+			get_tree().quit()
+
+#func _input(event):
+	#if event.is_action_pressed("kp 2") || event.is_action_pressed("kp 8"): # down
+		#if selector==0:
+			#selector=1
+			#var child := $Start as RichTextLabel
+			#child.text = "Start"
+			#var child2 := $Exit as RichTextLabel
+			#child2.text = ">Exit"
+			##print("selector is %d", selector )
+		#else:
+			#selector=0
+			#var child := $Start as RichTextLabel
+			#child.text = ">Start"
+			#var child2 := $Exit as RichTextLabel
+			#child2.text = "Exit"
+			##print("selector is %d", selector )
+		#
+	#if event.is_action_pressed("kp enter"): # enter
+			#if selector==0:
+				#get_tree().change_scene_to_file("res://maingame.tscn")
+			#else:
+				#get_tree().quit()
